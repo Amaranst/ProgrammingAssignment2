cab208266861d2c5ac89e5d401fa596c96b4af71
Amaranst <Siripat@Siripats-MacBook-Pro.local>
2015-09-26 12:20:36 -0400

    assignment 2 cachemetrix

--- cachematrix.R
+++ cachematrix.R
@@ -7,9 +7,31 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
 ## Write a short comment describing this function
-
+# Create a special metrix above.
+# Then, Check the condition.
+# If it already exist, get the inverse metrix from cache
+# Otherwise, compute a new one
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # GET inverse metrix of x
+  ix <- x$getInverse()
+  
+  # CHECK if there is inverse metrix
+  if(!is.null(ix)) {
+    message("getting cached data")
+    return(ix)
+  }
+  
+  # GET metrix
+  m <- x$get()
+  
+  # CALCULATE inverse 
+  ix <- solve(m, ...)
+  
+  #SET inverse
+  x$setInverse(ix)
+  
+  ## Return a matrix that is the inverse of 'x'
+  ix
 }
